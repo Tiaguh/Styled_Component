ceae1e1d242aa91ba1981939d4c3576fd330bd9c
Tiaguh <tiagosingup@gmail.com>
2023-09-20 18:52:23 -0300

    implementing more things

--- src/Pages/Teste/Teste.jsx
+++ src/Pages/Teste/Teste.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import './Teste.css';
 
+import iconPlay from './img/play.png'
+
 export default function Teste() {
     const [number1, setNumber1] = useState(0);
     const [number2, setNumber2] = useState(0);
 
     const [response, setResponse] = useState('');
-
-    const [status, setStatus] = useState('');
+    // Resposta do usuário
 
     const [started, setStarted] = useState(false);
+    // Controlar se o jogo já começou
+
+    const [changeClassName, setChangeClassName] = useState(true)
+
+    console.log(changeClassName);
 
     const result = number1 * number2;
 
@@ -28,38 +34,51 @@ export default function Teste() {
         sortNumbers();
         setStarted(true);
         setResponse('');
-        setStatus('');
     }
 
     function checkResponse(e) {
         e.preventDefault();
 
         if (response === result) {
-            setStatus('Correto!');
+            sortNumbers()
+            setResponse('')
+            setChangeClassName(true)
         } else {
-            setStatus('Errado!');
+            setResponse('')
+            setChangeClassName(false)
         }
     }
 
     return (
         <div className="container">
-            <div className="ask">
-                <p>{number1}</p>
-                <p>*</p>
-                <p>{number2}</p>
-            </div>
 
-            <form onSubmit={checkResponse}>
-                <input
-                    value={response}
-                    onChange={(e) => setResponse(parseInt(e.target.value))}
-                    type="number"
-                />
-            </form>
+            {started ? (
+                <div className="game-container">
 
-            <h1>{status}</h1>
+                    <div className="question">
+                        <p>{number1}</p>
+                        <p>*</p>
+                        <p>{number2}</p>
+                    </div>
 
-            <button onClick={handleStart}>Start</button>
+                    <form onSubmit={checkResponse}>
+                        <input
+                            value={response}
+                            onChange={(e) => setResponse(parseInt(e.target.value))}
+                            type="number"
+
+                            className={
+                                changeClassName ? "input-correct" : "input-uncorrect"
+                            }
+
+                        />
+                    </form>
+                </div>
+            ) : (
+                <button onClick={handleStart}>
+                    <img src={iconPlay} alt="icon-play" />
+                </button>
+            )}
         </div>
     );
 }
