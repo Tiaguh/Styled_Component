53623bbc98e4270b2c13b9658f52e344e51cbeb7
Tiaguh <tiagosingup@gmail.com>
2023-09-19 23:04:05 -0300

    improve the system

--- src/Pages/Teste/Teste.jsx
+++ src/Pages/Teste/Teste.jsx
@@ -1,52 +1,63 @@
 import React, { useState } from 'react';
-import "./Teste.css";
+import './Teste.css';
 
 export default function Teste() {
     const [number1, setNumber1] = useState(0);
     const [number2, setNumber2] = useState(0);
 
-    const [response, setResponse] = useState("");
+    const [response, setResponse] = useState('');
 
-    const [round, setRound] = useState(1)
-    
+    const [status, setStatus] = useState('');
+
+    const [started, setStarted] = useState(false);
+
     const result = number1 * number2;
 
-    console.log(number1, number2);
-    console.log(response);
+    function sortNumbers() {
+        setNumber1(Math.floor(Math.random() * 10) + 1);
+        setNumber2(Math.floor(Math.random() * 10) + 1);
+    }
 
-    function sort() {
-        setNumber1(parseInt(Math.random() * 11));
-        setNumber2(parseInt(Math.random() * 11));
+    // Math.floor vai me gerar um número entre 0 e 9.
+    // Somando + 1 a esse resultado eu impeço que o resultado seja 0.
+    // Pois se o número for 0 vai ser somado a mais + 1 de qualquer jeito.
+
+    function handleStart() {
+        sortNumbers();
+        setStarted(true);
+        setResponse('');
+        setStatus('');
     }
 
     function checkResponse(e) {
         e.preventDefault();
 
-        console.log(result);
-
-        if (parseInt(response) === result) {
-            alert("Correto!");
+        if (response === result) {
+            setStatus('Correto!');
         } else {
-            alert("Errado!");
+            setStatus('Errado!');
         }
-
-        setRound(round + 1)
     }
 
     return (
         <div className="container">
-
-            <p> Rodada {round}</p>
-            
             <div className="ask">
                 <p>{number1}</p>
-                <p>.</p>
+                <p>*</p>
                 <p>{number2}</p>
             </div>
 
-            <input onChange={(e) => setResponse(e.target.value)} type="number" />
-            <button onClick={sort}>Sort</button>
-            <button onClick={checkResponse}>Check</button>
+            <form onSubmit={checkResponse}>
+                <input
+                    value={response}
+                    onChange={(e) => setResponse(e.target.value)}
+                    type="number"
+                />
+            </form>
+
+            <h1>{status}</h1>
+
+            <button onClick={handleStart}>Start</button>
         </div>
     );
 }
